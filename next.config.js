8f26f9e2408afce5841d47eb129449d05f1060ef
Wajahat Ali Khan <assim345678@gmail.com>
2025-01-12 04:11:05 +0500

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['677fc83f0476123f76a8134b.mockapi.io'], // Replace with the domain of your image URLs
+  },
+};
+
+module.exports = nextConfig;
