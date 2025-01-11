cb7e693b5483a6553df8f7b9699d2ad1ea25867f
Wajahat Ali Khan <assim345678@gmail.com>
2025-01-12 03:30:20 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        reactStrictMode: true,
-        images: {
-          domains: ['www.themealdb.com'], // Add the hostname here
-        },
+    reactStrictMode: true,
+    images: {
+      domains: ['www.themealdb.com', '677fc83f0476123f76a8134b.mockapi.io'],
+    },
 };
-
 export default nextConfig;
