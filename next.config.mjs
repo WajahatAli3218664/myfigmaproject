232ec9643b338ec305151f80c97b6778d8b8b928
Wajahat Ali Khan <assim345678@gmail.com>
2025-01-12 03:48:08 +0500

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+        reactStrictMode: true,
+        images: {
+          domains: ['https://mockapi.io'], // Add the hostname here
+        },
+};
+
+export default nextConfig;
+
